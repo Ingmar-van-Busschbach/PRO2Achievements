89e92ebf7e36417fdfad366cd86e0fec523783d5
Ingmar van Busschbach <30147@ma-web.nl>
2019-09-13 11:22:48 +0200

    Reorganized files, unzipped

---
+++ Assignments/Assignment 2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace opdracht2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("###########################");
+            Console.WriteLine("#####      Round 1     ####");
+            Console.WriteLine("###########################");
+
+            int dwStartTime = System.Environment.TickCount;
+
+            while (true)
+
+            {
+
+                if (System.Environment.TickCount - dwStartTime > 500) break; //1000 milliseconds 
+
+            }
+
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("###########################");
+            Console.WriteLine("#####      Round 1     ####");
+            Console.WriteLine("###########################");
+
+            while (true)
+
+            {
+
+                if (System.Environment.TickCount - dwStartTime > 1000) break; //1000 milliseconds 
+
+            }
+
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("###########################");
+            Console.WriteLine("#####      Round 1     ####");
+            Console.WriteLine("###########################");
+
+            while (true)
+
+            {
+
+                if (System.Environment.TickCount - dwStartTime > 1500) break; //1000 milliseconds 
+
+            }
+
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("###########################");
+            Console.WriteLine("#####      Round 1     ####");
+            Console.WriteLine("###########################");
+
+            while (true)
+
+            {
+
+                if (System.Environment.TickCount - dwStartTime > 2500) break; //1000 milliseconds 
+
+            }
+
+            Console.Clear();
+
+            Console.WriteLine("###########################");
+            Console.WriteLine("#####      FIGHT!!     ####");
+            Console.WriteLine("###########################");
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Assignments/Assignment 3/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace opdracht2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Clear();
+
+            Console.WriteLine("Insert your name");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello", name);
+
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("###########################");
+            Console.WriteLine("#####      Round 1     ####");
+            Console.WriteLine("###########################");
+
+            int dwStartTime = System.Environment.TickCount;
+
+            while (true)
+
+            {
+
+                if (System.Environment.TickCount - dwStartTime > 500) break; //1000 milliseconds 
+
+            }
+
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("###########################");
+            Console.WriteLine("#####      Round 1     ####");
+            Console.WriteLine("###########################");
+
+            while (true)
+
+            {
+
+                if (System.Environment.TickCount - dwStartTime > 1000) break; //1000 milliseconds 
+
+            }
+
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("###########################");
+            Console.WriteLine("#####  BOT vs {0}  ###", name);
+            Console.WriteLine("###########################");
+
+            while (true)
+
+            {
+
+                if (System.Environment.TickCount - dwStartTime > 1500) break; //1000 milliseconds 
+
+            }
+
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("###########################");
+            Console.WriteLine("#####  BOT vs {0}  ###", name);
+            Console.WriteLine("###########################");
+
+            while (true)
+
+            {
+
+                if (System.Environment.TickCount - dwStartTime > 2500) break; //1000 milliseconds 
+
+            }
+
+            Console.Clear();
+
+            Console.WriteLine("###########################");
+            Console.WriteLine("#####      FIGHT!!     ####");
+            Console.WriteLine("###########################");
+
+            Console.ReadLine();
+
+        }
+    }
+}
