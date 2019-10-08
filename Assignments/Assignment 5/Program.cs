929148eb0a3efd0deef271971c3f18b014a70cd6
Ingmar-van-Busschbach <30147@ma-web.nl>
2019-10-08 09:18:59 +0200

    Added assignment 7

---
+++ Assignments/Assignment 5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace opdracht7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberA = 0;
+            int numberB = 0;
+
+            numberA ++;
+            ++numberA;
+            numberA = numberA + 1;
+
+            numberB--;
+            --numberB;
+            numberB = numberB - 1;
+
+            if (numberA==3 && numberB==-3)
+            {
+                Console.WriteLine("Number A: {0}", numberA);
+                Console.WriteLine("Number B: {0}", numberB);
+                Console.WriteLine("Calculation Complete");
+            }
+            else
+            {
+                Console.WriteLine("Calculation Failed");
+            }
+        }
+    }
+}
